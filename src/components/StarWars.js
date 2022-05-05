58aa798e83c363f01bd4a94dff37602bc8628e66
SerhiiPetrenk0 <suation196@gmail.com>
2022-05-05 18:24:09 +0300

    React Grid - Fundamentals

---
+++ src/components/StarWars.js
@@ -0,0 +1,125 @@
+import React, { useState, useEffect }  from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-bootstrap4';
+import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
+import  ArrowBottom from './img/ArrowBottom'
+export const StartWars = () => {
+  const [ list, setList ] = useState([])
+  const renderList = list.map((item, index) => <ListGroup.Item style={{background: item.eye_color}} key={index}>{item.name}</ListGroup.Item>)
+
+  const columns = [
+    { name: 'id', title: 'ID' },
+    { name: 'name', title: 'Name' },
+    { name: 'height', title: 'Height' },
+    { name: 'mass', title: 'Mass' },
+    { name: 'hair_color', title: 'Hair_color' },
+    { name: 'skin_color', title: 'Skin_color' },
+    { name: 'eye_color', title: 'Eye_color' },
+    { name: 'birth_year', title: 'Birth_year' },
+    { name: 'gender', title: 'Gender' }
+  ];
+
+  const forColumns = columns.map(item => ({columnName: item.name, align: 'center',  wordWrapEnabled: true }))
+  const [tableColumnExtensions] = useState(forColumns);
+
+  const test = list.map((item, index) => ({ 
+    id: index, 
+    name: item.name, 
+    height: item.height, 
+    mass: item.mass, 
+    hair_color: item.hair_color, 
+    skin_color: item.skin_color, 
+    eye_color: item.eye_color, 
+    birth_year: item.birth_year, 
+    gender: item.gender
+  }))
+  const TableComponent = ({ ...restProps }) => (
+    <Table.Table
+      {...restProps}
+      className="table-striped "
+    />
+  );
+
+  const getStarWarsAPI = () => {
+    const URL = 'https://swapi.dev/api/'
+    const people = 'people/'
+    fetch(`${URL}${people}`)
+        .then(respone => respone.json())
+        .then(result => setList(result.results))
+        .catch(err => console.error(err))
+  }
+  useEffect(() => {
+      getStarWarsAPI()
+  },[])
+  const HighlightedCell = ({ value, style, ...restProps }) => (
+    <Table.Cell
+      {...restProps}
+      style={{
+        backgroundColor: value > 180 ? 'red' : value < 100 ? 'green' : undefined,
+        ...style,
+      }}
+    >
+      <span
+        style={{
+          color: value > 180 ? 'white' : undefined,
+        }}
+      >
+        {value}
+      </span>
+    </Table.Cell>
+  );
+  const Cell = (props) => {
+    const { column } = props;
+    if (column.name === 'height') {
+      return <HighlightedCell {...props} />;
+    }
+    return <Table.Cell {...props} />;
+  };
+  const TableRow = ({ row, ...restProps }) => (
+    <Table.Row
+      {...restProps}
+      onClick={() => alert(JSON.stringify(row))}
+    />
+  );
+  const TableHeaderContent = ({ column, children, ...restProps }) => (
+    <TableHeaderRow.Content
+      column={column}
+      {...restProps}
+    >
+      {children}
+      {column.name === 'name' ? (
+        <button
+          type="button"
+          className="btn btn-outline-secondary"
+          style={{ margin: '-7px 5px' }}
+          onClick={() => alert('Custom action')}
+        >
+          <ArrowBottom />
+        </button>
+      ) : null}
+    </TableHeaderRow.Content>
+  );
+  const renderTable =  ( 
+    <div className="card">
+      <Grid
+        rows={test}
+        columns={columns}
+      >
+        <Table 
+          rowComponent={TableRow}
+          cellComponent={Cell}
+          tableComponent={TableComponent} 
+          columnExtensions={tableColumnExtensions}
+          
+        />
+        <TableHeaderRow
+          contentComponent={TableHeaderContent}
+        />
+      </Grid>
+    </div>
+  )
+  return (
+    <div>
+      {renderTable}
+    </div>)
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
