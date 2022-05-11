8bd0df0701748a202bb663a6ec0b2cdc9762a8be
SerhiiPetrenk0 <suation196@gmail.com>
2022-05-11 22:38:59 +0300

    Add Drawer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import React, { useState } from 'react';
+import { Header } from './components/Header';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import { DrawerLeft } from './components/Drawer'
+import { HeaderDrawer } from './components/HeaderDrawer'
+export default function App() {
+  const [ DrawerOpen, setDraverOpen ] = useState(false)
+  return (<>
+        <HeaderDrawer />
+{/*         <Header setDraverOpen={setDraverOpen} />
+        <DrawerLeft DrawerOpen={DrawerOpen} /> */}
+  </>
   );
 }
-
-export default App;
